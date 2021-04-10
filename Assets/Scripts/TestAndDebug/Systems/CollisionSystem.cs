80d0d7023e979822d89daa6d4fa17f6e9de07fac
epender <epender@umich.edu>
2021-04-10 11:55:45 -0400

    Fix friction bug

--- Assets/Scripts/TestAndDebug/Systems/CollisionSystem.cs
+++ Assets/Scripts/TestAndDebug/Systems/CollisionSystem.cs
@@ -23,17 +23,6 @@ public class CollisionSystem : SystemBase {
             Box box1 = boxes[this.box1];
             Box box2 = boxes[this.box2];
 
-            // Ideally, we would have a float6 J, but the library only goes up
-            // to float4, so J is split into 2 pieces.
-            float3 J1 = new float3(-normal, -Lin.Cross(contact-box1.pos, normal));
-            float3 J2 = new float3(normal, Lin.Cross(contact-box2.pos, normal));
-
-            float2 tangent = Lin.Cross(normal, -1);
-
-            float3 J1_t = new float3(tangent, Lin.Cross(contact-box1.pos, tangent));
-            float3 J2_t = new float3(-tangent, -Lin.Cross(contact-box2.pos, tangent));
-            
-            // Similar situation with M
             float3x3 M1_inv = new float3x3(
                 1/box1.mass, 0, 0,
                 0, 1/box1.mass, 0,
@@ -45,53 +34,96 @@ public class CollisionSystem : SystemBase {
                 0, 0, 1/box2.inertia
             );
 
-            float3 v1 = new float3(box1.vel, box1.angVel);
-            float3 v2 = new float3(box2.vel, box2.angVel);
+            {
+                // Ideally, we would have a float6 J, but the library only goes up
+                // to float4, so J is split into 2 pieces.
+                float3 J1 = new float3(-normal, -Lin.Cross(contact-box1.pos, normal));
+                float3 J2 = new float3(normal, Lin.Cross(contact-box2.pos, normal));
 
-            float m_c = 1 / (math.dot(math.mul(J1, M1_inv), J1) + math.dot(math.mul(J2, M2_inv), J2));
-            float m_t = 1 / (math.dot(math.mul(J1_t, M1_inv), J1_t) + math.dot(math.mul(J2_t, M2_inv), J2_t));
 
-            float beta = .3f;
-            float delta_slop = -.05f;
-            float delta = -Geometry.GetOverlapOnAxis(box1.ToRect(), box2.ToRect(), normal);
+                float3 v1 = new float3(box1.vel, box1.angVel);
+                float3 v2 = new float3(box2.vel, box2.angVel);
 
-            float bias = 0;
+                float m_c = 1 / (math.dot(math.mul(J1, M1_inv), J1) + math.dot(math.mul(J2, M2_inv), J2));
 
-            if (delta < delta_slop) {
-                bias = beta/dt * (delta - delta_slop);
+                float beta = .3f;
+                float delta_slop = -.005f;
+                float delta = -Geometry.GetOverlapOnAxis(box1.ToRect(), box2.ToRect(), normal);
+
+                float bias = 0;
+
+                if (delta < delta_slop) {
+                    bias = (beta/dt) * (delta - delta_slop);
+                }
+
+                float lambda = -m_c * (math.dot(J1, v1) + math.dot(J2, v2) + bias);
+
+                ClampLambda(ref lambda);
+
+                // Impulse
+                float3 P_c1 = J1*lambda;
+                float3 P_c2 = J2*lambda;
+
+                // Delta velocity
+                float3 dv1 = math.mul(M1_inv, P_c1);
+                float3 dv2 = math.mul(M2_inv, P_c2);
+
+                box1.vel += dv1.xy;
+                box1.angVel += dv1.z;
+
+                box2.vel += dv2.xy;
+                box2.angVel += dv2.z;
             }
 
-            float lambda = -m_c * (math.dot(J1, v1) + math.dot(J2, v2) + bias);
-            float lambda_t = -m_t * (math.dot(J1_t, v1) + math.dot(J2_t, v2));
+            ////////////////////////////////
+            /////////// Friction ///////////
+            ////////////////////////////////
 
-            // Frictional coefficient
-            float mu = .5f;
-            ClampLambdas(ref lambda, ref lambda_t, mu);
+            {
+                float2 tangent = Lin.Cross(normal, -1);
 
-            // Impulse
-            float3 P_c1 = J1*lambda + J1_t*lambda_t;
-            float3 P_c2 = J2*lambda + J2_t*lambda_t;
+                float3 J1_t = new float3(tangent, Lin.Cross(contact-box1.pos, tangent));
+                float3 J2_t = new float3(-tangent, -Lin.Cross(contact-box2.pos, tangent));
+                
+                float m_t = 1 / (math.dot(math.mul(J1_t, M1_inv), J1_t) + math.dot(math.mul(J2_t, M2_inv), J2_t));
 
-            // Delta velocity
-            float3 dv1 = math.mul(M1_inv, P_c1);
-            float3 dv2 = math.mul(M2_inv, P_c2);
+                float3 v1 = new float3(box1.vel, box1.angVel);
+                float3 v2 = new float3(box2.vel, box2.angVel);
 
-            box1.vel += dv1.xy;
-            box1.angVel += dv1.z;
+                float lambda_t = -m_t * (math.dot(J1_t, v1) + math.dot(J2_t, v2));
 
-            box2.vel += dv2.xy;
-            box2.angVel += dv2.z;
+                // Frictional coefficient
+                float mu = .5f;
+
+                ClampLambda_t(ref lambda_t, mu);
+
+                float3 P_t1 = J1_t*lambda_t;
+                float3 P_t2 = J2_t*lambda_t;
+
+                // Delta velocity
+                float3 dv1 = math.mul(M1_inv, P_t1);
+                float3 dv2 = math.mul(M2_inv, P_t2);
+
+                box1.vel += dv1.xy;
+                box1.angVel += dv1.z;
+
+                box2.vel += dv2.xy;
+                box2.angVel += dv2.z;
+            }
 
             // NOTE: This is not thread safe
             boxes[this.box1] = box1;
             boxes[this.box2] = box2;
         }
 
-        private void ClampLambdas(ref float lambda, ref float lambda_t, float frictionCoefficient) {
+
+        private void ClampLambda(ref float lambda) {
             float oldAccumulated = lambdaAccumulated;
             lambdaAccumulated = math.max(lambdaAccumulated + lambda, 0);
             lambda = lambdaAccumulated - oldAccumulated;
+        }
 
+        private void ClampLambda_t(ref float lambda_t, float frictionCoefficient) {
             float old_tAccumulated = lambda_tAccumulated;
             lambda_tAccumulated = math.clamp(lambda_tAccumulated + lambda_t, -lambdaAccumulated*frictionCoefficient, lambdaAccumulated*frictionCoefficient);
             lambda_t = lambda_tAccumulated - old_tAccumulated;
@@ -104,6 +136,11 @@ public class CollisionSystem : SystemBase {
     protected override void OnCreate() {
         boxEntities = new NativeList<Entity>(100, Allocator.Persistent);
         boxBoxConstraints = new NativeList<BoxBoxConstraint>(100, Allocator.Persistent);
+    }
+
+    protected override void OnDestroy() {
+        boxEntities.Dispose();
+        boxBoxConstraints.Dispose();
     }
 
     protected override void OnUpdate() {
