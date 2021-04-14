56919a93488fceab1ce79caca007a95bb50c739a
epender <epender@umich.edu>
2021-04-14 15:40:00 -0400

    Move friction constraint into a more general struct

--- Assets/Scripts/TestAndDebug/Constraints/BoxBoxConstraint.cs
+++ Assets/Scripts/TestAndDebug/Constraints/BoxBoxConstraint.cs
@@ -25,12 +25,8 @@ public struct BoxBoxConstraint {
     float3 M1_inv;
     float3 M2_inv;
 
-    float3 J1_t;
-    float3 J2_t;
-
-    float m_c_t;
-
     PenetrationConstraint pc;
+    FrictionConstraint fc;
 
     public BoxBoxConstraint(Entity box1, Entity box2, float2 normal, Geometry.Contact contact) {
         this.box1 = box1;
@@ -43,11 +39,9 @@ public struct BoxBoxConstraint {
 
         M1_inv = 0;
         M2_inv = 0;
-        J1_t = 0;
-        J2_t = 0;
-        m_c_t = 0;
 
         pc = new PenetrationConstraint();
+        fc = new FrictionConstraint();
     }
 
     public void PreStep(ref ComponentDataFromEntity<Box> boxes, float dt, Lambdas prevLambdas) {
@@ -80,16 +74,15 @@ public struct BoxBoxConstraint {
         { // Tangent precomputation (friction)
             float2 tangent = Lin.Cross(normal, -1);
 
-            J1_t = new float3(tangent, Lin.Cross(contact-box1.pos, tangent));
-            J2_t = new float3(-tangent, -Lin.Cross(contact-box2.pos, tangent));
+            float3 J1_t = new float3(tangent, Lin.Cross(contact-box1.pos, tangent));
+            float3 J2_t = new float3(-tangent, -Lin.Cross(contact-box2.pos, tangent));
 
-            m_c_t = 1 / (math.dot(J1_t * M1_inv, J1_t) + math.dot(J2_t * M2_inv, J2_t));
+            fc = new FrictionConstraint(J1_t, J2_t, M1_inv, M2_inv, CollisionSystem.globalFriction);
         }
 
         if (CollisionSystem.accumulateImpulses) {
             (float3 P1_n, float3 P2_n) = pc.GetImpulse(accum.n);
-            float3 P1_t = J1_t*accum.t;
-            float3 P2_t = J2_t*accum.t;
+            (float3 P1_t, float3 P2_t) = fc.GetImpulse(accum.t);
 
             ApplyImpulse(P1_n + P1_t, ref box1);
             ApplyImpulse(P2_n + P2_t, ref box2);
@@ -124,15 +117,7 @@ public struct BoxBoxConstraint {
             float3 v1 = new float3(box1.vel, box1.angVel);
             float3 v2 = new float3(box2.vel, box2.angVel);
 
-            float lambda_t = -m_c_t * (math.dot(J1_t, v1) + math.dot(J2_t, v2));
-
-            // Frictional coefficient
-            float mu = CollisionSystem.globalFriction;
-
-            ClampLambda_t(ref lambda_t, lambda,  mu, ref accum.t, ref accum.n);
-
-            float3 P_t1 = J1_t*lambda_t;
-            float3 P_t2 = J2_t*lambda_t;
+            (float3 P_t1, float3 P_t2) = fc.GetImpulse(v1, v2, lambda, ref accum.t, ref accum.n);
 
             ApplyImpulse(P_t1, ref box1);
             ApplyImpulse(P_t2, ref box2);
@@ -144,31 +129,9 @@ public struct BoxBoxConstraint {
     }
 
     private static void ApplyImpulse(float3 impulse, ref Box box) {
-        // Delta velocity
         float3 dv = new float3(1/box.mass, 1/box.mass, 1/box.inertia) * impulse;
 
         box.vel += dv.xy;
         box.angVel += dv.z;
-    }
-
-
-    private static void ClampLambda(ref float lambda, ref float accumulated) {
-        if (CollisionSystem.accumulateImpulses) {
-            float oldAccumulated = accumulated;
-            accumulated = math.max(accumulated + lambda, 0);
-            lambda = accumulated - oldAccumulated;
-        } else {
-            lambda = math.max(lambda, 0);
-        }
-    }
-
-    private static void ClampLambda_t(ref float lambda_t, float lambda_n, float frictionCoefficient, ref float accum_t, ref float accum_n) {
-        if (CollisionSystem.accumulateImpulses) {
-            float old_tAccumulated = accum_t;
-            accum_t = math.clamp(accum_t + lambda_t, -accum_n*frictionCoefficient, accum_n*frictionCoefficient);
-            lambda_t = accum_t - old_tAccumulated;
-        } else {
-            lambda_t = math.clamp(lambda_t, -lambda_n*frictionCoefficient, lambda_n*frictionCoefficient);
-        }
     }
 }
--- Assets/Scripts/TestAndDebug/Constraints/GenericConstraint.cs
+++ Assets/Scripts/TestAndDebug/Constraints/GenericConstraint.cs
@@ -5,12 +5,12 @@ using Physics.Math;
 
 using ContactId = Physics.Math.Geometry.ContactId;
 
-public  struct PenetrationConstraint {
-    public  float3 J1;
-    public  float3 J2;
-    public  float m_c;
+public readonly struct PenetrationConstraint {
+    public readonly float3 J1;
+    public readonly float3 J2;
+    public readonly float m_c;
 
-    public  float bias;
+    public readonly float bias;
 
     public PenetrationConstraint(float3 J1, float3 J2, float3 M1_inv, float3 M2_inv, float bias) {
         this.J1 = J1;
@@ -44,6 +44,46 @@ public  struct PenetrationConstraint {
             lambda = accumulated - oldAccumulated;
         } else {
             lambda = math.max(lambda, 0);
+        }
+    }
+}
+
+
+
+public readonly struct FrictionConstraint {
+    public readonly float3 J1;
+    public readonly float3 J2;
+    public readonly float m_c;
+    public readonly float frictionCoeff;
+
+    public FrictionConstraint(float3 J1, float3 J2, float3 M1_inv, float3 M2_inv, float frictionCoeff) {
+        this.J1 = J1;
+        this.J2 = J2;
+    
+        m_c = 1 / (math.dot(J1 * M1_inv, J1) + math.dot(J2 * M2_inv, J2));
+
+        this.frictionCoeff = frictionCoeff;
+    }
+
+    public (float3, float3) GetImpulse(float lambda) {
+        return (J1*lambda, J2*lambda);
+    }
+    
+    public (float3, float3) GetImpulse(float3 v1, float3 v2, float lambda_n, ref float accum_t, ref float accum_n) {
+        float lambda_t = -m_c * (math.dot(J1, v1) + math.dot(J2, v2));
+
+        ClampLambda(ref lambda_t, lambda_n, ref accum_t, ref accum_n);
+
+        return (J1*lambda_t, J2*lambda_t);
+    }
+    
+    private void ClampLambda(ref float lambda_t, float lambda_n, ref float accum_t, ref float accum_n) {
+        if (CollisionSystem.accumulateImpulses) {
+            float old_tAccumulated = accum_t;
+            accum_t = math.clamp(accum_t + lambda_t, -accum_n*frictionCoeff, accum_n*frictionCoeff);
+            lambda_t = accum_t - old_tAccumulated;
+        } else {
+            lambda_t = math.clamp(lambda_t, -lambda_n*frictionCoeff, lambda_n*frictionCoeff);
         }
     }
 }
