41074081afa6a60eee1e922bd3f6134b440def72
epender <epender@umich.edu>
2021-04-24 17:46:48 -0400

    Refactor shadow edge generator to use light managers

--- Assets/Scripts/Physics/Systems/ShadowEdgeGenerationSystem.cs
+++ Assets/Scripts/Physics/Systems/ShadowEdgeGenerationSystem.cs
@@ -8,6 +8,8 @@ using UnityEngine;
 
 using Rect = Physics.Math.Rect;
 
+using ShadowEdge = ShadowEdgeGenerationSystem.ShadowEdge;
+
 [UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
 [UpdateAfter(typeof(GravitySystem))]
 public class ShadowEdgeGenerationSystem : SystemBase {
@@ -20,56 +22,107 @@ public class ShadowEdgeGenerationSystem : SystemBase {
         public Rect collider;
         public float2 lightSource;
     }
-    NativeList<ShadowEdge> shadowEdges;
+
+    List<LightManager> lightManagers;
 
     protected override void OnCreate() {
-        shadowEdges = new NativeList<ShadowEdge>(Allocator.Persistent);
         lightSourceQuery = GetEntityQuery(typeof(LightSource));
+        lightManagers = new List<LightManager>();
     }
 
     protected override void OnDestroy() {
-        shadowEdges.Dispose();
+        Clear(lightManagers);
     }
-    
+
     protected override void OnUpdate() {
-        var shadowEdges = this.shadowEdges;
-        shadowEdges.Clear();
+        Clear(lightManagers);
+
         var lightSources = lightSourceQuery.ToComponentDataArray<LightSource>(Allocator.TempJob);
 
-        foreach (var l in lightSources) {
-            float2 lightPos = l.pos;
-            Entities
-                .WithAll<OpaqueObject>()
-                .ForEach((in Box box, in Entity entity) => {
-                    Geometry.CalculateShadowGeometry(box.ToRect(), lightPos, .05f, out var sg1, out var sg2);
-                    shadowEdges.Add(new ShadowEdge {
-                        contact1 = sg1.contact1,
-                        contact2 = sg1.contact2,
-                        opaque = entity,
-                        collider = Rect.FromLineSegment(sg1.contact1, sg1.contact1 + math.normalize(sg1.contact1 - lightPos)*20, sg1.id),
-                        lightSource = lightPos
-                    });
-                    shadowEdges.Add(new ShadowEdge {
-                        contact1 = sg2.contact1,
-                        contact2 = sg2.contact2,
-                        opaque = entity,
-                        collider = Rect.FromLineSegment(sg2.contact1, sg2.contact1 + math.normalize(sg2.contact1 - lightPos)*20, sg2.id),
-                        lightSource = lightPos
-                    });
-                }
-            ).Run();
+        foreach (var lightSource in lightSources) {
+            lightManagers.Add(new LightManager(in lightSource));
         }
-
+        
+        Entities
+            .WithAll<OpaqueObject>()
+            .WithoutBurst()
+            .ForEach((in Box box, in Entity entity) => {
+                foreach (var l in lightManagers) {
+                    l.Add(in box, in entity);
+                }
+            }).Run();
+        
+        foreach (var lightManager in lightManagers) {
+            lightManager.Compute();
+        }
+        
         lightSources.Dispose();
     }
 
     public IEnumerable<ShadowEdge> GetShadowEdgesForDebug() {
-        foreach (var edge in shadowEdges) {
+        foreach (var edge in lightManagers[0].shadowEdges) {
             yield return edge;
         }
     }
 
     public NativeList<ShadowEdge> GetShadowEdges() {
-        return shadowEdges;
+        // TODO: For now, there is only one light manager
+        return lightManagers[0].shadowEdges;
+    }
+
+    private void Clear(List<LightManager> lightManagers) {
+        foreach (var lightManager in lightManagers) {
+            lightManager.Dispose();
+        }
+        lightManagers.Clear();
+    }
+
+}
+
+public class LightManager {
+    private LightSource source;
+    public NativeList<ShadowEdge> shadowEdges {get;}
+
+    private struct ShadowEdgeData {
+        float2 contact1;
+        float2? contact2;
+        int id;
+    }
+    private struct ProtoEdge {
+        float angle;
+        Entity opaque;
+    }
+
+    public LightManager(in LightSource source) {
+        this.source = source;
+        shadowEdges = new NativeList<ShadowEdge>(Allocator.TempJob);
+    }
+
+    public void Dispose() {
+        shadowEdges.Dispose();
+    }
+
+    public void Add(in Box box, in Entity entity) {
+        // TODO: For the purposes of calculation, we could use a smaller struct.
+        float2 lightPos = source.pos;
+        Geometry.CalculateShadowGeometry(box.ToRect(), lightPos, .05f, out var sg1, out var sg2);
+        shadowEdges.Add(new ShadowEdge {
+            contact1 = sg1.contact1,
+            contact2 = sg1.contact2,
+            opaque = entity,
+            collider = Rect.FromLineSegment(sg1.contact1, sg1.contact1 + math.normalize(sg1.contact1 - lightPos)*20, sg1.id),
+            lightSource = lightPos
+        });
+        shadowEdges.Add(new ShadowEdge {
+            contact1 = sg2.contact1,
+            contact2 = sg2.contact2,
+            opaque = entity,
+            collider = Rect.FromLineSegment(sg2.contact1, sg2.contact1 + math.normalize(sg2.contact1 - lightPos)*20, sg2.id),
+            lightSource = lightPos
+        });
+    }
+
+    public void Compute() {
+        
     }
 }
