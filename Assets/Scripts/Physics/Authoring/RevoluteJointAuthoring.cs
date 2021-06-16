c25e8b75b806179930acd776e1ec816f74449d41
epender <epender@umich.edu>
2021-06-16 11:59:17 -0400

    Improve RevoluteJointAuthoring
    Allow starting the scene with the spring stretched

--- Assets/Scripts/Physics/Authoring/RevoluteJointAuthoring.cs
+++ Assets/Scripts/Physics/Authoring/RevoluteJointAuthoring.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
+
 using Unity.Entities;
 using Unity.Mathematics;
 
 using UnityEngine;
+using UnityEditor;
 
 public class RevoluteJointAuthoring : MonoBehaviour, IConvertGameObjectToEntity {
     public GameObject obj1;
@@ -10,32 +13,49 @@ public class RevoluteJointAuthoring : MonoBehaviour, IConvertGameObjectToEntity 
     public float beta;
     public float softness;
 
+    public float2 offset;
+
+    public Entity e;
 
     public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem) {
 
         float2 x1 = (Vector2)obj1.transform.position;
         float2 x2 = (Vector2)obj2.transform.position;
 
-        Vector2 WorldToLocal(GameObject obj, Vector2 point) {
-            point -= (Vector2)obj.transform.position;
-            point = obj.transform.InverseTransformDirection(point);
-            return point;
-        }
-
         conversionSystem.GetPrimaryEntity(obj1);
         dstManager.AddComponentData(entity, new RevoluteJoint{
             e1 = conversionSystem.GetPrimaryEntity(obj1),
             e2 = conversionSystem.GetPrimaryEntity(obj2),
-            r1 = WorldToLocal(obj1, transform.position),
-            r2 = WorldToLocal(obj2, transform.position),
+            r1 = GetR1(),
+            r2 = GetR2(),
             beta = beta,
             softness = softness
         });
     }
 
+    Vector2 WorldToLocal(GameObject obj, Vector2 point) {
+        point -= (Vector2)obj.transform.position;
+        point = obj.transform.InverseTransformDirection(point);
+        return point;
+    }
+
+    Vector2 GetR1() {
+        return WorldToLocal(obj1, (Vector2)transform.position - (Vector2)offset/2);
+    }
+    Vector2 GetR2() {
+        return WorldToLocal(obj2, (Vector2)transform.position + (Vector2)offset/2);
+    }
+
     void OnDrawGizmos() {
-        Gizmos.DrawSphere(transform.position, .1f);
-        Gizmos.DrawLine(obj1.transform.position, transform.position);
-        Gizmos.DrawLine(obj2.transform.position, transform.position);
+        Vector2 x1 = obj1.transform.position;
+        Vector2 x2 = obj2.transform.position;
+
+        Gizmos.DrawSphere(x1 + GetR1(), .1f);
+        Gizmos.DrawRay(x1, GetR1());
+        Gizmos.DrawSphere(x2 + GetR2(), .1f);
+        Gizmos.DrawRay(x2, GetR2());
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(x1 + GetR1(), x2 + GetR2());
     }
 }
